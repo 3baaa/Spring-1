9eda995f76bfe5db1c9a0fd50bacf2e942e10ce8
b3aaa <rodrlehqk@naver.com>
2021-05-25 18:19:16 +0900

    JSON 응답과 요청 처리

---
+++ sp5-chap16(JSON 응답과 요청 처리)/src/main/java/config/MvcConfig.java
@@ -0,0 +1,81 @@
+package config;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import controller.RegisterRequestValidator;
+import interceptor.AuthCheckInterceptor;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authCheckInterceptor()).addPathPatterns("/edit/**");
+	}
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().featuresToEnable(SerializationFeature.INDENT_OUTPUT).deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(formatter)).simpleDateFormat("yyyyMMdd HHmmss").build();
+		//ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter)).build();
+		//ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().simpleDateFormat("yyyyMMddHHmmss").build();
+		//ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).build();
+		converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
+	}
+	
+	@Override
+	public Validator getValidator() {
+		return new RegisterRequestValidator();
+	}
+	
+	@Bean
+	public AuthCheckInterceptor authCheckInterceptor() {
+		return new AuthCheckInterceptor();
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasenames("message.label");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+}
